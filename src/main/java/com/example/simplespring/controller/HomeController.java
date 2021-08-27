79fa1f3c7c562939d8156f9f58487867089871cf
Pavan Hegde <pavanrkadave@gmail.com>
2021-08-27 16:09:59 +0530

    Initial Commit

---
+++ src/main/java/com/example/simplespring/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.simplespring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String getHome(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "index";
+	}
+}
